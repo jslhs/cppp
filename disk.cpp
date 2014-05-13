81b12d1887feffc18762bd36662f315abe5ea129
jslhs <aclazycat@gmail.com>
2014-05-13 18:29:45 +0800

    implement exec_ata_cmd
    using ata pass-through & scsi ata pass-through approach

--- disk.cpp
+++ disk.cpp
@@ -1,10 +1,23 @@
 #include "disk.h"
 #include <sstream>
+#include <algorithm>
+#include <cctype>
+#include <bitset>
+#include <memory>
+#include <winerror.h>
+#pragma comment(lib, "Ws2_32")
 
 namespace utility
 {
 	namespace detail
 	{
+		inline std::string trim(const std::string &s)
+		{
+			auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c){return std::isspace(c); });
+			auto wsback = std::find_if_not(s.rbegin(), s.rend(), [](int c){return std::isspace(c); }).base();
+			return (wsback <= wsfront ? std::string() : std::string(wsfront, wsback));
+		}
+
 		handle_wrapper::handle_wrapper(HANDLE handle, std::string &err)
 			: _handle(handle)
 			, _err(err)
@@ -20,6 +33,118 @@ namespace utility
 		handle_wrapper::operator HANDLE() const
 		{
 			return _handle;
+		}
+
+		bool exec_ata_cmd(HANDLE hdev, const ata_command &cmd, void *buf, uint32_t transfer_length, int flags, int timeout_seconds)
+		{
+			ata_status status = 0;
+			DWORD bytes_returned;
+			BOOL rv;
+
+			auto cached_buf = (char *)VirtualAlloc(nullptr, transfer_length, MEM_COMMIT, PAGE_READWRITE);
+			if (!cached_buf) return false;
+
+			ATA_PASS_THROUGH_DIRECT param{}, out{};
+			param.Length = sizeof(ATA_PASS_THROUGH_DIRECT);
+			param.AtaFlags = flags;
+			param.DataBuffer = cached_buf;
+			param.DataTransferLength = transfer_length;
+			param.TimeOutValue = timeout_seconds;
+			auto &taskfile = param.CurrentTaskFile;
+			taskfile[0] = cmd.feature & 0xff;
+			taskfile[1] = cmd.count & 0xff;
+			taskfile[2] = cmd.lba & 0xff;
+			taskfile[3] = (cmd.lba >> 8) & 0xff;
+			taskfile[4] = (cmd.lba >> 16) & 0xff;
+			taskfile[5] = cmd.device & 0xff;
+			taskfile[6] = cmd.cmd & 0xff;
+
+			rv = DeviceIoControl(hdev
+				, IOCTL_ATA_PASS_THROUGH_DIRECT
+				, &param
+				, sizeof(param)
+				, &out
+				, sizeof(out)
+				, &bytes_returned
+				, nullptr);
+			status = out.CurrentTaskFile[6];
+			auto err = GetLastError();
+
+			if (err == ERROR_NOT_SUPPORTED)
+			{
+				//SCSI_PASS_THROUGH_DIRECT sp{}, sp_out{};
+				auto bufsize = sizeof(SCSI_PASS_THROUGH_DIRECT) + 32;
+				std::unique_ptr<char[]> buf(new char[bufsize]);
+				SCSI_PASS_THROUGH_DIRECT &sp = *reinterpret_cast<SCSI_PASS_THROUGH_DIRECT *>(buf.get());
+				sp.Length = sizeof(SCSI_PASS_THROUGH_DIRECT);
+				sp.DataBuffer = cached_buf;
+				sp.DataTransferLength = transfer_length;
+				sp.TimeOutValue = timeout_seconds;
+				sp.CdbLength = 12;
+				sp.SenseInfoLength = 32;
+				sp.SenseInfoOffset = sp.Length;
+				sp.Cdb[0] = 0xa1; // ATA PASS-THROUGH(12)
+
+				char *sense = (char *)buf.get() + sp.Length;
+				
+				int proto = ata_proto_reserved;
+				int dir = 0;
+				bool use_dma = ((flags & ata_use_dma) == ata_use_dma);
+				if (flags & ata_data_in)
+				{
+					proto = use_dma ? ata_proto_udma_data_in : ata_proto_pio_data_in;
+					sp.DataIn = SCSI_IOCTL_DATA_IN;
+					dir = 1;
+				}
+				else if (flags & ata_data_out)
+				{
+					proto = use_dma ? ata_proto_udma_data_out : ata_proto_pio_data_out;
+					sp.DataIn = SCSI_IOCTL_DATA_OUT;
+				}
+				else
+				{
+					sp.DataIn = SCSI_IOCTL_DATA_UNSPECIFIED;
+				}
+
+				sp.Cdb[1] = proto << 1;
+				sp.Cdb[2] = (0x1 << 5) | (dir << 3) | (0x1 << 2) | 0x02; // transfer length in SECTOR_COUNT field
+				sp.Cdb[3] = cmd.feature & 0xff;
+				sp.Cdb[4] = cmd.count & 0xff;
+				sp.Cdb[5] = cmd.lba & 0xff;
+				sp.Cdb[6] = (cmd.lba >> 8) & 0xff;
+				sp.Cdb[7] = (cmd.lba >> 16) & 0xff;
+				sp.Cdb[8] = cmd.device & 0xff;
+				sp.Cdb[9] = cmd.cmd & 0xff;
+
+				rv = DeviceIoControl(hdev
+					, IOCTL_SCSI_PASS_THROUGH_DIRECT
+					, buf.get()
+					, bufsize
+					, buf.get()
+					, bufsize
+					, &bytes_returned
+					, nullptr);
+				err = GetLastError();
+				if (err || sp.ScsiStatus)
+				{
+					if (sense[1] != 0x1)
+					{
+						status = 1;
+					}
+					else
+					{
+						status = sense[21];
+						err = 0;
+					}
+				}
+			}
+
+			memcpy_s(buf, transfer_length, cached_buf, transfer_length);
+
+			rv = VirtualFree(cached_buf, 0, MEM_RELEASE);
+			SetLastError(err);
+
+			return !(status & ata_mask_error);
 		}
 	}
 
@@ -43,7 +168,7 @@ namespace utility
 	{
 		using namespace detail;
 		auto h = CreateFileA(_filename.c_str()
-			, GENERIC_READ
+			, GENERIC_READ | GENERIC_WRITE
 			, FILE_SHARE_READ | FILE_SHARE_WRITE
 			, nullptr
 			, OPEN_EXISTING
@@ -73,11 +198,12 @@ namespace utility
 		if (rv)
 		{
 			_size = geo.DiskSize.QuadPart;
+			_log_bytes_per_sector = geo.Geometry.BytesPerSector;
 		}
 
 		int bufsize = sizeof(DRIVE_LAYOUT_INFORMATION_EX) + kwin32_max_parts * sizeof(PARTITION_INFORMATION_EX);
-		char *buf = new char[bufsize];
-		DRIVE_LAYOUT_INFORMATION_EX *di = reinterpret_cast<DRIVE_LAYOUT_INFORMATION_EX *>(buf);
+		std::unique_ptr<char[]> buf(new char[bufsize]);
+		DRIVE_LAYOUT_INFORMATION_EX *di = reinterpret_cast<DRIVE_LAYOUT_INFORMATION_EX *>(buf.get());
 		rv = DeviceIoControl(handle_wrapper(h, _err)
 			, IOCTL_DISK_GET_DRIVE_LAYOUT_EX
 			, nullptr
@@ -88,10 +214,87 @@ namespace utility
 			, nullptr);
 		if (rv)
 		{
+			for (DWORD i = 0; i < di->PartitionCount; i++)
+			{
+				auto &part = di->PartitionEntry[i];
+				partition p;
+				p.number = part.PartitionNumber;
+				p.starting_offset = part.StartingOffset.QuadPart;
+				p.length = part.PartitionLength.QuadPart;
+				p.unused = (part.PartitionStyle == PARTITION_STYLE_MBR) 
+					&& (part.Mbr.PartitionType == PARTITION_ENTRY_UNUSED);
+				_parts.push_back(p);
+			}
+		}
+
+		STORAGE_PROPERTY_QUERY q{};
+		q.PropertyId = StorageDeviceProperty;
+		q.QueryType = PropertyStandardQuery;
+
+		STORAGE_DESCRIPTOR_HEADER hdr{};
+		rv = DeviceIoControl(handle_wrapper(h, _err)
+			, IOCTL_STORAGE_QUERY_PROPERTY
+			, &q
+			, sizeof(q)
+			, &hdr
+			, sizeof(hdr)
+			, &bytes_returned
+			, nullptr);
+
+		if (rv)
+		{
+			std::unique_ptr<char[]> buf(new char[hdr.Size]);
+			rv = DeviceIoControl(handle_wrapper(h, _err)
+				, IOCTL_STORAGE_QUERY_PROPERTY
+				, &q
+				, sizeof(q)
+				, buf.get()
+				, hdr.Size
+				, &bytes_returned
+				, nullptr);
+
+			STORAGE_DEVICE_DESCRIPTOR *sdd = reinterpret_cast<STORAGE_DEVICE_DESCRIPTOR *>(buf.get());
 
 		}
 
-		delete[] buf;
+		// ATA IDENTIFY DEVICE APPROACH
+		ata_command cmd{};
+		cmd.cmd = 0xec; // IDENTIFY DEVICE
+		cmd.count = 1;
+		uint16_t data[256];
+
+		const int word_serial_start = 10;
+		const int word_serial_count = 10;
+		const int word_firm_start = 23;
+		const int word_firm_count = 4;
+		const int word_model_start = 27;
+		const int word_model_count = 20;
+
+		typedef std::pair<int, int> range;
+		range ranges[] = {
+			{ word_serial_start, word_serial_start + word_serial_count }, // serial number
+			{ word_firm_start, word_firm_start + word_firm_count }, // firmware revision
+			{ word_model_start, word_model_start + word_model_count }, // model number
+		}; 
+		
+		auto s = exec_ata_cmd(handle_wrapper(h, _err), cmd, data, sizeof(data), ata_data_in);
+	
+		for (auto r : ranges)
+		{
+			for (int i = r.first; i < r.second; i++)
+				data[i] = ntohs(data[i]);
+		}
+
+		_serial = trim(std::string((char *)&data[word_serial_start], word_serial_count * 2));
+		_firm_rev = trim(std::string((char *)&data[word_firm_start], word_firm_count * 2));
+		_model = trim(std::string((char *)&data[word_model_start], word_model_count * 2));
+
+		auto word106 = std::bitset<16>(data[106]);
+		_log_bytes_per_sector = word106.test(12) ? 
+			(*reinterpret_cast<uint32_t *>(&data[107])) : 256 * 2;
+		_phy_bytes_per_sector = _log_bytes_per_sector << (data[106] & 0xf);
+
+		disk_size_t max_lba = *reinterpret_cast<disk_size_t *>(&data[100]);
 	}
 
 	int disk::number() const
@@ -102,6 +305,18 @@ namespace utility
 	disk_size_t disk::size() const
 	{
 		return _size;
+	}
+
+	// logical sector size, bytes per sector
+	sector_size_t disk::sector_size() const
+	{
+		return _log_bytes_per_sector;
+	}
+
+	// physical sector size, bytes per sector
+	sector_size_t disk::phy_sector_size() const
+	{
+		return _phy_bytes_per_sector;
 	}
 
 	bool disk::exists() const
--- disk.h
+++ disk.h
@@ -7,13 +7,16 @@
 
 #define WIN32_MEAN_AND_LEAN
 #include <Windows.h>
+#include <ntddscsi.h>
 
 namespace utility
 {
 	using disk_size_t = uint64_t;
+	using sector_size_t = uint32_t;
 
 	struct partition
 	{
+		int number;
 		disk_size_t starting_offset;
 		disk_size_t length;
 		bool unused;
@@ -47,6 +50,24 @@ namespace utility
 
 		using ata_status = uint32_t;
 
+		enum ata_protocols
+		{
+			ata_proto_hard_reset,
+			ata_proto_srst,
+			ata_proto_reserved,
+			ata_proto_non_data,
+			ata_proto_pio_data_in,
+			ata_proto_pio_data_out,
+			ata_proto_pio_dma,
+			ata_proto_pio_dma_queued,
+			ata_proto_device_diagnostic,
+			ata_proto_device_reset,
+			ata_proto_udma_data_in,
+			ata_proto_udma_data_out,
+			ata_proto_fpdma,
+			ata_return_response_info = 15
+		};
+
 		enum ata_status_mask
 		{
 			ata_mask_error = 0x1,
@@ -57,12 +78,15 @@ namespace utility
 
 		enum ata_flags
 		{
-			ata_drdy_required,
-			ata_data_in,
-			ata_48bit_command,
-			ata_use_dma,
-			ata_no_multiple,
+			ata_drdy_required =			ATA_FLAGS_DRDY_REQUIRED,
+			ata_data_in =				ATA_FLAGS_DATA_IN,
+			ata_data_out =				ATA_FLAGS_DATA_OUT,
+			ata_48bit_command =			ATA_FLAGS_48BIT_COMMAND,
+			ata_use_dma =				ATA_FLAGS_USE_DMA,
+			ata_no_multiple =			ATA_FLAGS_NO_MULTIPLE,
 		};
+
+		bool exec_ata_cmd(HANDLE hdev, const ata_command &cmd, void *buf, uint32_t transfer_length, int flags, int timeout_seconds = 1);
 	}
 
 	enum disk_layout
@@ -83,7 +107,14 @@ namespace utility
 		bool exists() const;
 		int number() const;
 		disk_size_t size() const;
-		const std::string &vendor() const;
+
+		// logical sector size, bytes per sector
+		sector_size_t sector_size() const;
+
+		// physical sector size, bytes per sector
+		sector_size_t phy_sector_size() const;
+
+		const std::string &model() const;
 		const partition_set &parts() const;
 		disk_layout layout() const;
 
@@ -92,17 +123,19 @@ namespace utility
 		bool format();
 
 	private:
-		detail::ata_status exec_ata_cmd(const detail::ata_command &cmd, uint32_t transfer_length, void *buf, detail::ata_flags flags);
 		void identify();
 
 	private:
 		int _num;
 		bool _exists;
+		sector_size_t _log_bytes_per_sector; // logical bytes/sector
+		sector_size_t _phy_bytes_per_sector; // physical bytes/sector
 		disk_size_t _size;
 		std::string _err;
 		std::string _filename;
-		std::string _vendor;
+		std::string _serial;
 		std::string _model;
+		std::string _firm_rev;
 		partition_set _parts;
 	};
 
